f42fc784dea83ff28fe5bd757d3e6328f6735190
Michael Samiec <michaelsamiec@improbable.io>
2018-11-20 15:29:17 +0000

    Dynamically resolve singletons

--- SpatialGDKEditorToolbar/Source/Private/SchemaGenerator/SpatialGDKEditorSchemaGenerator.cpp
+++ SpatialGDKEditorToolbar/Source/Private/SchemaGenerator/SpatialGDKEditorSchemaGenerator.cpp
@@ -124,24 +124,24 @@ TArray<UClass*> GetAllSupportedClasses()
 {
 	TSet<UClass*> Classes;
 
-	for (TObjectIterator<UClass> ClassIt; ClassIt; ++ClassIt)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("MCS: Class: %s %d"), *ClassIt->GetName(), (int)ClassIt->GetSpatialClassFlags());
+	//for (TObjectIterator<UClass> ClassIt; ClassIt; ++ClassIt)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("MCS: Class: %s %d"), *ClassIt->GetName(), (int)ClassIt->GetSpatialClassFlags());
 
-		if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_None))
-		{
-			UE_LOG(LogTemp, Warning, TEXT("MCS: Spatial class none: %s"), *ClassIt->GetName());
-		}
-		if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_NotSpatialType))
-		{
-			UE_LOG(LogTemp, Warning, TEXT("MCS: Not spatial type: %s"), *ClassIt->GetName());
-		}
-		if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_Singleton))
-		{
-			UE_LOG(LogTemp, Warning, TEXT("MCS: Singleton type: %s"), *ClassIt->GetName());
-		}
-	}
-	return Classes;
+	//	if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_None))
+	//	{
+	//		UE_LOG(LogTemp, Warning, TEXT("MCS: Spatial class none: %s"), *ClassIt->GetName());
+	//	}
+	//	if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_NotSpatialType))
+	//	{
+	//		UE_LOG(LogTemp, Warning, TEXT("MCS: Not spatial type: %s"), *ClassIt->GetName());
+	//	}
+	//	if (ClassIt->HasAnySpatialClassFlags(SPATIALCLASS_Singleton))
+	//	{
+	//		UE_LOG(LogTemp, Warning, TEXT("MCS: Singleton type: %s"), *ClassIt->GetName());
+	//	}
+	//}
+	//return Classes.Array();
 
 	for (TObjectIterator<UClass> ClassIt; ClassIt; ++ClassIt)
 	{
@@ -171,7 +171,9 @@ TArray<UClass*> GetAllSupportedClasses()
 
 		// Ensure we don't process skeleton or reinitialized classes
 		if (SupportedClass->GetName().StartsWith(TEXT("SKEL_"), ESearchCase::CaseSensitive)
-			|| SupportedClass->GetName().StartsWith(TEXT("REINST_"), ESearchCase::CaseSensitive))
+			|| SupportedClass->GetName().StartsWith(TEXT("REINST_"), ESearchCase::CaseSensitive)
+			|| SupportedClass->GetName().StartsWith(TEXT("TRASHCLASS_"), ESearchCase::CaseSensitive)
+			|| SupportedClass->GetName().StartsWith(TEXT("HOTRELOADED_"), ESearchCase::CaseSensitive))
 		{
 			continue;
 		}
@@ -195,7 +197,7 @@ bool SpatialGDKGenerateSchema()
 
 	SchemaGeneratedClasses.Sort();
 
-	FString SchemaOutputPath = SpatialGDKToolbarSettings->GetGeneratedSchemaOutputFolder();
+	FString SchemaOutputPath = GetDefault<USpatialGDKEditorToolbarSettings>()->GetGeneratedSchemaOutputFolder();
 
 	UE_LOG(LogSpatialGDKSchemaGenerator, Display, TEXT("Schema path %s"), *SchemaOutputPath);
 
